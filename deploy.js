8e79446f579b00b45617c66c7ebb6fc3d5821c96
Pablo Lopes <96208410+pablolteixeira@users.noreply.github.com>
2022-10-15 21:06:41 -0300

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,8 +3,8 @@ const Web3 = require('web3');
 const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'SEED PHRASE HERE',
-  'INFURA GOERLI LINK HERE'
+  'A',
+  'B'
 );
 const web3 = new Web3(provider);
 
